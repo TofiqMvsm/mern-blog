c7359b71378be44001971fbc95c1950d048faf3a
WIN-6LUN2D0CH7K\user <tofiqmvsm2003@gmail.com>
2023-12-24 14:13:02 +0400

    Creating Register and Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+  });
 
-export default Login
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    <section className="login">
+      <div className="container">
+        <h2>Sign In</h2>
+        <form className="form login-form">
+          <p className="form-error-message">This is a error message</p>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+
+          <button type="submit" className="btn primary">
+            Register
+          </button>
+        </form>
+        <small>
+          Don't have an account? <Link to="/register">Sign up</Link>
+        </small>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
