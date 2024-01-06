5aa745e134ad8910fc502ecacf5b4d3112a8e4e6
TofiqMvsm <tofiqmvsm2003@gmail.com>
2024-01-06 14:49:10 +0400

    Delete post and edit post with axios

--- client/src/pages/DeletePost.jsx
+++ client/src/pages/DeletePost.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from 'react'
 import { UserContext } from '../context/UserContext'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate,useLocation } from 'react-router-dom'
+import axios from 'axios'
 
-const DeletePost = () => {
+const DeletePost = ({postId : id}) => {
   const {currentUser} = useContext(UserContext)
   const token = currentUser?.token
   const navigate = useNavigate()
@@ -13,8 +14,26 @@ const DeletePost = () => {
       navigate('/login')
     }
   },[])
+  const location = useLocation()
+
+  const removePost =async ()=>{
+    try {
+        const response = await axios.delete(`${process.env.REACT_APP_BASE_URL}/posts/${id}`,{withCredentials : true, headers : {Authorization : `Bearer ${token}`}})
+        if(response.status == 200){
+          if(location.pathname == `myPosts/${currentUser.id}`){
+            navigate(0)
+          }
+          else{
+            navigate('/')
+          }
+        }
+    } catch (err) {
+        console.log('Couldnt delete post')
+    }
+  }
+
   return (
-    <Link className='btn sm danger'>Delete</Link>
+    <Link className='btn sm danger' onClick={()=>removePost(id)}>Delete</Link>
   )
 }
 
--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -2,16 +2,20 @@ import React, { useContext, useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { UserContext } from "../context/UserContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios"
 const EditPost = () => {
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("Uncategorized");
   const [description, setDescription] = useState("");
   const [thumbnail, setThumbnail] = useState("");
+  const [error,setError] = useState('')
 
+  // console.log(title)
+  
   const modules = {
     toolbar: [
-      [{ header: [1, 2,3,4,5,6,false] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
       ["bold", "italic", "underline", "strike", "blockquote"],
       [
         { list: "ordered" },
@@ -24,23 +28,30 @@ const EditPost = () => {
     ],
   };
 
-  const {currentUser} = useContext(UserContext)
-  const token = currentUser?.token
-  const navigate = useNavigate()
+  const { currentUser } = useContext(UserContext);
+  const token = currentUser?.token;
+  const navigate = useNavigate();
 
   //redirect to login page for any user who isn't logged in
-  useEffect(()=>{
-    if(!token){
-      navigate('/login')
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
     }
-  },[])
-
-
+  }, []);
 
   const formats = [
-    'header','bold','italic','underline','strike','blockquote','list','bullet','indent','link','image'
-  ]
-
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
 
   const POST_CATEGORIES = [
     "Agriculture",
@@ -52,13 +63,59 @@ const EditPost = () => {
     "Uncategorized",
     "Weather",
   ];
+  const { id } = useParams();
+
+  useEffect(()=>{
+    const getPost = async ()=>{
+      try {
+          const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/${id}`)
+          setDescription(response.data.description)
+          setTitle(response.data.title)
+       console.log(response.data.title)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getPost()
+  },[])
+
+
+
+  const editPost = async (e)=>{
+    e.preventDefault()
+    const postData = new FormData()
+    postData.set('title',title)
+    postData.set('category',category)
+    postData.set('description',description)
+    postData.set('thumbnail',thumbnail)
+
+    try {
+      const response = await axios.patch(`${process.env.REACT_APP_BASE_URL}/posts/${id}`,postData, {withCredentials : true ,headers : {Authorization : `Bearer ${token}`}})
+      console.log(response.data) 
+      if(response.status == 200){
+        console.log('Saasas')
+        return navigate('/')
+         
+      }
+      else{
+        console.log("if e girmedi")
+      }
+      console.log("sasasa");
+    } catch (error) {
+       setError(error.response.data.message)
+    }
+
+
+  }
+
 
   return (
     <section className="create-post">
       <div className="container">
         <h2>Edit Post</h2>
-        <p className="form-error-message">This is an error message</p>
-        <form className="form create-post-form">
+        {error && <p className="form-error-message">{error}</p>}
+        <form className="form create-post-form" onSubmit={editPost}>
           <input
             type="text"
             placeholder="Title"
@@ -69,13 +126,18 @@ const EditPost = () => {
           <select
             name="category"
             value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            onChange={(e) => setCategory(e.target.value)} 
           >
             {POST_CATEGORIES.map((cat) => (
               <option key={cat}>{cat}</option>
             ))}
           </select>
-          <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription} />
+          <ReactQuill
+            modules={modules}
+            formats={formats}
+            value={description}
+            onChange={setDescription}
+          />
           <input
             type="file"
             onChange={(e) => setThumbnail(e.target.files[0])}
